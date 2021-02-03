98d6bbba29e1c658acc3788c143243fe0ad0a7c4
nblack0917 <nickablack@gmail.com>
2021-02-03 14:51:41 -0600

    added pagination

--- src/components/Import.js
+++ src/components/Import.js
@@ -10,11 +10,19 @@ import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
 import IconButton from '@material-ui/core/IconButton';
 import Fade from '@material-ui/core/Fade';
+import TableFooter from '@material-ui/core/TableFooter';
+import TablePagination from '@material-ui/core/TablePagination';
+import FirstPageIcon from '@material-ui/icons/FirstPage';
+import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
+import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
+import LastPageIcon from '@material-ui/icons/LastPage';
 
 
 const Import = (props) => {
     const [anchorEl, setAnchorEl] = React.useState(null);
     const [curIndex, setCurIndex] = React.useState(null);
+    const [page, setPage] = React.useState(0);
+    const [rowsPerPage, setRowsPerPage] = React.useState(10);
     const open = Boolean(anchorEl);
 
     const handleClick = (index) => (event) => {
@@ -30,6 +38,15 @@ const Import = (props) => {
 
     const handleClose = (props) => {
         setAnchorEl(null);
+    };
+
+    const handleChangePage = (event, newPage) => {
+        setPage(newPage);
+    };
+    
+    const handleChangeRowsPerPage = (event) => {
+        setRowsPerPage(parseInt(event.target.value, 10));
+        setPage(0);
     };
 
 
@@ -48,7 +65,8 @@ const Import = (props) => {
                     </TableRow>
                 </TableHead>
                 <TableBody>
-                    {props.makes.map((make, index) => (
+                    {props.makes.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                        .map((make, index) => (
                         <TableRow key={make.MakeName + index}>
                         <TableCell component="th" scope="row" align="left">
                             {make.MakeId}
@@ -68,6 +86,20 @@ const Import = (props) => {
                         </TableRow>
                     ))}
                 </TableBody>
+                <TableFooter>
+                    <TableRow>
+                        <TablePagination
+                        rowsPerPageOptions={[5, 10, 25]}
+                        colSpan={3}
+                        count={props.makes.length}
+                        rowsPerPage={rowsPerPage}
+                        page={page}
+                        onChangePage={handleChangePage}
+                        onChangeRowsPerPage={handleChangeRowsPerPage}
+                        // ActionsComponent={TablePaginationActions}
+                        />
+                    </TableRow>
+                </TableFooter>
             </Table>
             <Menu
                 id="fade-menu"
